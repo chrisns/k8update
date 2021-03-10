7909b4bf30baf35404b5084a255670b5fe2a4413
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-03-10 14:38:15 +0000

    fix linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es6': true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
   },
-  'extends': [
+  extends: [
     'standard'
   ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  'parserOptions': {
-    'ecmaVersion': 2018
+  parserOptions: {
+    ecmaVersion: 2018
   },
-  'rules': {
+  rules: {
   }
 }
