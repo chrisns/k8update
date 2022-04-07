4374c43647e39bcd4b5e53a86db30282bdaf74fa
Chris Nesbitt-Smith <chris@cns.me.uk>
2022-04-07 16:40:45 +0100

    fix linting

--- index.js
+++ index.js
@@ -8,21 +8,41 @@ const CoreV1Api = kc.makeApiClient(k8s.CoreV1Api)
 
 const labelselector = process.env.LABELSELECTOR || null
 const cache = []
-CoreV1Api.listPodForAllNamespaces(null, null, null, labelselector).then(pods =>
-  pods.body.items.map(pod => {
-    if (pod.status.phase !== 'Running') { return }
-    pod.status.containerStatuses.map((container, i) => {
-      const runningSHA = container.imageID.split('@').pop(-1)
-      const image = pod.spec.containers[i].image.split('@')[0]
-      const latestSHA = cache[image] ? cache[image] : JSON.parse(execSync(`/usr/bin/skopeo inspect --tls-verify=false docker://${image}`).toString()).Digest
-
-      cache[image] = latestSHA
-      console.log(pod.metadata.name, container.name, runningSHA, latestSHA, (runningSHA === latestSHA) ? 'matched' : 'not matched')
-      if (runningSHA !== latestSHA) {
-        CoreV1Api.deleteNamespacedPod(pod.metadata.name, pod.metadata.namespace).then(deletedPod =>
-          console.log(`${pod.metadata.namespace}/${pod.metadata.name} didn't match, it is no more`)
-        )
+CoreV1Api.listPodForAllNamespaces(null, null, null, labelselector).then(
+  (pods) =>
+    pods.body.items.forEach((pod) => {
+      if (pod.status.phase !== 'Running') {
+        return
       }
+      pod.status.containerStatuses.forEach((container, i) => {
+        const runningSHA = container.imageID.split('@').pop(-1)
+        const image = pod.spec.containers[i].image.split('@')[0]
+        const latestSHA = cache[image]
+          ? cache[image]
+          : JSON.parse(
+            execSync(
+                `/usr/bin/skopeo inspect --tls-verify=false docker://${image}`
+            ).toString()
+          ).Digest
+
+        cache[image] = latestSHA
+        console.log(
+          pod.metadata.name,
+          container.name,
+          runningSHA,
+          latestSHA,
+          runningSHA === latestSHA ? 'matched' : 'not matched'
+        )
+        if (runningSHA !== latestSHA) {
+          CoreV1Api.deleteNamespacedPod(
+            pod.metadata.name,
+            pod.metadata.namespace
+          ).then((deletedPod) =>
+            console.log(
+              `${pod.metadata.namespace}/${pod.metadata.name} didn't match, it is no more`
+            )
+          )
+        }
+      })
     })
-  })
 )
